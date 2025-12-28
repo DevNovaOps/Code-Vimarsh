8d20eda5f7b4b044568ee89b2b3381166bfda8f7
DevNovaOps <topthings2006@gmail.com>
2025-12-28 22:39:08 +0530

    Configure database settings for local and Railway environments

--- codevimarsh/settings.py
+++ codevimarsh/settings.py
@@ -66,20 +66,39 @@ WSGI_APPLICATION = 'codevimarsh.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MYSQLDATABASE"),
-        "USER": os.getenv("MYSQLUSER"),
-        "PASSWORD": os.getenv("MYSQLPASSWORD"),
-        "HOST": os.getenv("MYSQLHOST"),
-        "PORT": os.getenv("MYSQLPORT", "3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
+IS_RAILWAY = bool(os.getenv("RAILWAY_ENVIRONMENT"))
+
+if IS_RAILWAY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("MYSQLDATABASE"),
+            "USER": os.getenv("MYSQLUSER"),
+            "PASSWORD": os.getenv("MYSQLPASSWORD"),
+            "HOST": os.getenv("MYSQLHOST"),
+            "PORT": os.getenv("MYSQLPORT", "3306"),
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                "charset": "utf8mb4",
         },
     }
 }
+else:
+    DATABASES = {  
+    'default':
+      {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'cv',  
+        'USER': 'root',  
+        'PASSWORD': 'dev@2006',  
+        'HOST': 'localhost',  
+        'PORT': '3306', 
+        'OPTIONS':{
+            'charset': 'utf8mb4',
+            'init_command': "SET NAMES utf8mb4  COLLATE utf8mb4_unicode_ci",
+        },
+    }  
+}  
 
 
 # Password validation
@@ -105,7 +124,9 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 ALLOWED_HOSTS = [
-    "code-vimarsh-production.up.railway.app"
+    "code-vimarsh-production.up.railway.app",
+    '127.0.0.1',
+    'localhost',
 ]
 
 # Internationalization
